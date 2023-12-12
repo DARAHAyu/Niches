56a945a06aac6071f47a3cb48c7092d451148412
Yuichiro <yuantianyouyilang97@gmail.com>
2023-12-12 10:38:21 +0900

    user profile page

---
+++ app/Http/Controllers/UsersController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use Illuminate\Support\Facades\Auth;
+use App\Models\User;
+
+class UsersController extends Controller
+{
+    public function show($id)
+    {
+        // idの値でユーザを検索して取得
+        $user = User::findOrFail($id);
+
+        // ユーザ詳細ビューでそれを表示
+        return view('users.show', [
+            'user' => $user,
+        ]);
+    }
+}
--- resources/views/commons/link_items.blade.php
+++ resources/views/commons/link_items.blade.php
@@ -1,8 +1,12 @@
 @if (Auth::check())
-    {{-- ユーザ一覧ページへのリンク --}}
-    <li><a class="link link-hover" href="#">ユーザー</a></li>
-    {{-- ユーザ詳細ページへのリンク --}}
-    <li><a class="link link-hover" href="#">{{ Auth::user()->name }}&#39;のプロフィール</a></li>
+    {{-- ユーザプロフィールページへのリンク --}}
+    <li><a class="link link-hover" href="{{ route('users.show', Auth::user()->id) }}">{{ Auth::user()->name }}のプロフィール</a></li>
+    {{-- 発注者ページへのリンク --}}
+    <li><a class = "link link-hover" href="#">仕事を依頼する</a></li>
+    {{-- 受注者ページへのリンク --}}
+    <li><a class = "link link-hover" href="#">仕事を受注する</a></li>
+    {{-- タイムラインへのリンク（トップページとくっつけてもいいかも？） --}}
+    <li><a class = "link link-hover" href="#">仕事を探す</a></li>
     <li class="divider lg:hidden"></li>
     {{-- ログアウトへのリンク --}}
     <li><a class="link link-hover" href="#" onclick="event.preventDefault();this.closest('form').submit();">ログアウト</a></li>
