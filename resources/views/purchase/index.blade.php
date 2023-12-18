afd40a0b33b99a1a0743180e61d8b56592cda673
Yuichiro <yuantianyouyilang97@gmail.com>
2023-12-18 11:52:42 +0900

    Add button to create sales in mySales index page

--- resources/views/purchase/index.blade.php
+++ resources/views/purchase/index.blade.php
@@ -1,16 +1,48 @@
 @extends('layouts.app')
 
 @section('content')
-    @if (isset($myPurchase))
-        @foreach ($myPurchase as $myPurchase)
+    @if (Auth::id() == $user->id)
+        @if (isset($myPurchase))
             <div class="sm:grid sm:grid-cols-3 sm:gap-10">
+                <aside class="mt-4">
+                    @include('users.card')
+                    <a href="{{ route('purchase-create', $user->id) }}" class = "btn btn-primary btn-block normal-case">提案文を作成する</a>
+                </aside>
                 <div class="sm:col-span-2 mt-4">
-                    <p>作成日時：{{ $myPurchase->created_at }}</p>
-                    <p>最終更新日：{{ $myPurchase->updated_at }}</p>
-                    <p>タイトル：{{ $myPurchase->title }}</p>
-                    <p>提案の概要：{{ $myPurchase->purchase_abstract }}</p>
+                    @foreach ($myPurchase as $myPurchase)
+                        <div class="mt-4">
+                            <div class="sm:col-span-2 mt-4">
+                                <p>作成日時：{{ $myPurchase->created_at }}</p>
+                                <p>最終更新日：{{ $myPurchase->updated_at }}</p>
+                                <p>タイトル：{{ $myPurchase->title }}</p>
+                                <p>提案の概要：{{ $myPurchase->purchase_abstract }}</p>
+                            </div>
+                        </div>
+                    @endforeach
                 </div>
             </div>
-        @endforeach
+        @endif
     @endif
+@endsection    
+
+
+
+
+{{-- 
+@extends('layouts.app')
+
+@section('content')
+        @if (isset($myPurchase))
+            @foreach ($myPurchase as $myPurchase)
+                    <div class="sm:grid sm:grid-cols-3 sm:gap-10">
+                        <div class="sm:col-span-2 mt-4">
+                            <p>作成日時：{{ $myPurchase->created_at }}</p>
+                            <p>最終更新日：{{ $myPurchase->updated_at }}</p>
+                            <p>タイトル：{{ $myPurchase->title }}</p>
+                            <p>提案の概要：{{ $myPurchase->purchase_abstract }}</p>
+                        </div>
+                    </div>
+            @endforeach
+        @endif
 @endsection
+    --}}
