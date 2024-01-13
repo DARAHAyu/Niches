eeb7e84c78d5e28a41e3d130aaa4b1f580e7b39f
Yuichiro <yuantianyouyilang97@gmail.com>
2024-01-13 15:11:13 +0900

    Update: update validation rule in SalesOrdersRequest

--- app/Http/Requests/SalesOrdersRequest.php
+++ app/Http/Requests/SalesOrdersRequest.php
@@ -34,7 +34,7 @@ class SalesOrdersRequest extends FormRequest
         return [
             'title' => 'required|max:2000',
             'sales_abstract' => 'required|max:2000',
-            'category' => 'required|max:50',
+            'category_id' => 'required|integer',
             'budget' => 'required|numeric',
             'schedule' => 'required|date',
         ];
