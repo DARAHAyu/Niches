0d7d0eb24e89174fe7e93e41300199e63fb87d56
Yuichiro <yuantianyouyilang97@gmail.com>
2024-01-13 15:09:34 +0900

    Add: add category id column to sales_orders table

---
+++ database/migrations/2024_01_13_145352_add_category_id_column_to_sales_orders_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('sales_orders', function (Blueprint $table) {
+            // 外部キー制約を削除
+            $table->dropForeign(['category']);
+            // categoryカラムを削除
+            $table->dropColumn('category');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('sales_orders', function (Blueprint $table) {
+            // category_idカラムを削除
+            $table->dropColumn('category_id');
+            
+            // 外部キー制約を削除
+            $table->dropForeign(['category_id']);
+        });
+    }
+};
