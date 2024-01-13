009a223bf861055909c661da69e86d0525d8819b
Yuichiro <yuantianyouyilang97@gmail.com>
2024-01-13 15:16:55 +0900

    Add: add relationship between SalesOrder and Category

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class Category extends Model
 {
     use HasFactory;
+
+    public function sales_orders()
+    {
+        return $this->hasMany(SalesOrder::class);
+    }
 }
