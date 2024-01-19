06864f8eb6c13ca06000e0a3a2e20b7924d79a8b
Yuichiro <yuantianyouyilang97@gmail.com>
2024-01-19 13:33:44 +0900

    Add: authorization related to UserDetails.edit

--- app/Http/Controllers/UserDetailsController.php
+++ app/Http/Controllers/UserDetailsController.php
@@ -49,6 +49,8 @@ class UserDetailsController extends Controller
 
         $latestUserDetail = $user->user_details()->latest()->first();
 
+        $this->authorize('edit', $latestUserDetail);
+
         return view('details.edit', [
             'user' => $user,
             'latestUserDetail' => $latestUserDetail
